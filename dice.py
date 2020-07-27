f41dd0f8b80a62476318ae4a303df679c9645b11
Edex01 <jorasiac@gmail.com>
2020-07-27 18:20:21 +0530

    err1

--- dice.py
+++ dice.py
@@ -8,40 +8,38 @@ root.geometry('400x400')
 
 def dice():
 
-    while True :
+    number = random.randint(1,6)
 
-        number = random.randint(1,6)
+    if number == 1:
 
-        if number == 1:
+        img = ImageTk.PhotoImage(Image.open("Dice face/1.png"))
+        panel = Label(root, image = img)
+        panel.pack(side = "bottom", fill = "both", expand = "yes")
 
-            img = ImageTk.PhotoImage(Image.open("Dice face/1.png"))
-            panel = Label(root, image = img)
-            panel.pack(side = "bottom", fill = "both", expand = "yes")
+    if number == 2:
+        img = ImageTk.PhotoImage(Image.open("Dice face/2.png"))
+        panel = Label(root, image = img)
+        panel.pack(side = "bottom", fill = "both", expand = "yes")
 
-        if number == 2:
-            img = ImageTk.PhotoImage(Image.open("Dice face/2.png"))
-            panel = Label(root, image = img)
-            panel.pack(side = "bottom", fill = "both", expand = "yes")
+    if number == 3:
+        img = ImageTk.PhotoImage(Image.open("Dice face/3.png"))
+        panel = Label(root, image = img)
+        panel.pack(side = "bottom", fill = "both", expand = "yes")
 
-        if number == 3:
-            img = ImageTk.PhotoImage(Image.open("Dice face/3.png"))
-            panel = Label(root, image = img)
-            panel.pack(side = "bottom", fill = "both", expand = "yes")
+    if number == 4:
+        img = ImageTk.PhotoImage(Image.open("Dice face/4.png"))
+        panel = Label(root, image = img)
+        panel.pack(side = "bottom", fill = "both", expand = "yes")
 
-        if number == 4:
-            img = ImageTk.PhotoImage(Image.open("Dice face/4.png"))
-            panel = Label(root, image = img)
-            panel.pack(side = "bottom", fill = "both", expand = "yes")
+    if number == 5:
+        img = ImageTk.PhotoImage(Image.open("Dice face/5.png"))
+        panel = Label(root, image = img)
+        panel.pack(side = "bottom", fill = "both", expand = "yes")
 
-        if number == 5:
-            img = ImageTk.PhotoImage(Image.open("Dice face/5.png"))
-            panel = Label(root, image = img)
-            panel.pack(side = "bottom", fill = "both", expand = "yes")
-
-        if number == 6:
-            img = ImageTk.PhotoImage(Image.open("Dice face/6.jpeg"))
-            panel = Label(root, image = img)
-            panel.pack(side = "bottom", fill = "both", expand = "yes")
+    if number == 6:
+        img = ImageTk.PhotoImage(Image.open("Dice face/6.jpeg"))
+        panel = Label(root, image = img)
+        panel.pack(side = "bottom", fill = "both", expand = "yes")
 
 roll = Button(root , text = "Roll Dice!", command = dice )
 roll.pack()
